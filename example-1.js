f6e22e14b92775b9534112b8507d4f27827665ab
eechava6 <eechava6@eafit.edu.co>
2020-06-20 12:17:06 -0500

    adding example 1

--- example-1.js
+++ example-1.js
@@ -1,8 +1,67 @@
 import {createAll, cleanConsole} from './data';
 const companies = createAll();
 
+const formatCompanies = ( companies ) => {
+  for (const company of companies) {
+    company.users = formatUsers(company.users);
+    company.name = capitalize(company.name);
+  }
+  companies.sort(compareLength);
+  return companies;
+};
+
+const formatUsers = ( users ) => {
+  return users.map((user) => {
+    // If undefined empty string
+    (user.firstName !== undefined) ? user.firstName = capitalize(user.firstName) : user.firstName = '';
+    (user.lastName !== undefined) ? user.lastName = capitalize(user.lastName) : user.lastName = '';
+    return user;
+  }).sort(compareNames);
+};
+
+const compareLength = (companyA, companyB) => {
+  // Use toUpperCase() to ignore character casing
+  if (companyA === undefined) return -1;
+  if (companyB === undefined) return 1;
+
+  const usersA = companyA.usersLength;
+  const usersB = companyB.usersLength;
+
+  let comparison = 0;
+  if (usersA > usersB) {
+    comparison = -1;
+  } else if (usersA < usersB) {
+    comparison = 1;
+  }
+  return comparison;
+};
+
+const compareNames = (userA, userB) => {
+  if (userA.firstName === '' || userA.lastName === '') return 1;
+  if (userB.firstName === '' || userB.lastName === '') return -1;
+
+
+  const nameA = `${userA.firstName.charAt(0)}${userA.lastName.charAt(0)}`;
+  const nameB = `${userB.firstName.charAt(0)}${userB.lastName.charAt(0)}`;
+
+  const comparison = 0;
+  if (nameA < nameB ) {
+    return -1;
+  } else if (nameA > nameB ) {
+    return 1;
+  }
+  return comparison;
+};
+
+// Since we don't have to verify if string is in
+// pascal case, converting first char is enough
+const capitalize = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
 cleanConsole(1, companies);
-console.log('---- EXAMPLE 1 --- ', 'Put here your function');
+console.log('---- EXAMPLE 1 --- ', formatCompanies(companies));
+
 
 // -----------------------------------------------------------------------------
 // INSTRUCCIONES EN ESPAÑOL
